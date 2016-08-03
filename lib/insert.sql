26086a7ff9d98199716cb1e17bc6d2c53ddb678a
Tami <catquarks@gmail.com>
2016-08-03 17:02:35 -0400

    pass tests

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive)
+VALUES
+("Mr. Chocolate", 1, "M", "Brown", "Delicious", 0),
+("Rowdy", 15, "F", "Burgundy", "Raucoaus", 1),
+("Tabitha", 6, "M", "Silver", "Magical", 1),
+("Sergeant Brown", 20, "M", "Salt and Pepper", "Wise", 1),
+("Melissa", 17, "F", "Blonde", "Sullen", 0),
+("Grinch", 3, "M", "Green", "Humbug", 1),
+("Wendy", 10, "F", "Ginger", "Koala-Esque", 1),
+(null, 2, "M", "Burgundy", "Hungry", 0)
+;
